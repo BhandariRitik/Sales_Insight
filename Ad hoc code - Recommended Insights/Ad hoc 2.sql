27c58aac4bc55a0964c612c7d3e40f1d207242e9
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:49:16 +0530

    Ad hoc 2.sql

--- Ad hoc code - Recommended Insights/Ad hoc 2.sql
+++ Ad hoc code - Recommended Insights/Ad hoc 2.sql
@@ -1,14 +1,12 @@
-/* Store performance analysis */
-
-select 
- store_id, 
- city, 
- concat(round((sum(isu)/1000),2), 'K') as Incremental_Sold_unit
-from 
- revenue_ir_isu
-group by 
- store_id, 
- city
-order by 
- sum(isu) asc
-limit 10;
+SELECT 
+    store_id, 
+    city, 
+    CONCAT(ROUND((SUM(isu) / 1000), 2), 'K') AS Incremental_Sold_Unit 
+FROM 
+    revenue_ir_isu
+GROUP BY 
+    store_id, 
+    city
+ORDER BY 
+    SUM(isu) ASC
+LIMIT 10;
