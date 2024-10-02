8a894277703fe86405b58e04ef4c499fd38d6820
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:50:31 +0530

    Ad hoc 5.sql

--- Ad hoc code - Recommended Insights/Ad hoc 5.sql
+++ Ad hoc code - Recommended Insights/Ad hoc 5.sql
@@ -1,13 +1,12 @@
-/* Promotion type analysis */
 
-select 
- promo_type,
- round((sum(isu)/sum(`quantity_sold(before_promo)`) *100),2) as sold_qty_incremental_percentage,
- round((sum(IR)/sum(revenue_before_promo) * 100),2) as revenue_incremental_percentage
-from
- revenue_ir_isu
-group by 
- promo_type
-order by 
- revenue_incremental_percentage desc,
- sold_qty_incremental_percentage desc;
+SELECT 
+    promo_type,
+    ROUND((SUM(isu) / SUM(`quantity_sold(before_promo)`) * 100), 2) AS sold_qty_incremental_percentage,
+    ROUND((SUM(IR) / SUM(revenue_before_promo) * 100), 2) AS revenue_incremental_percentage
+FROM 
+    revenue_ir_isu
+GROUP BY 
+    promo_type
+ORDER BY 
+    revenue_incremental_percentage DESC,
+    sold_qty_incremental_percentage DESC;
