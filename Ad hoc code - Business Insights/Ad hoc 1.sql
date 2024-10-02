7bb10ae686fb539301542dd8b38fd2c6fd073eeb
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:46:14 +0530

    Ad hoc 1.sql

--- Ad hoc code - Business Insights/Ad hoc 1.sql
+++ Ad hoc code - Business Insights/Ad hoc 1.sql
@@ -1,10 +1,10 @@
-select 
-  distinct(product_name), 
-  product_code, 
-  promo_type, 
-  base_price 
-from 
-  revenue_ir_isu 
-where 
-  promo_type = 'BOGOF' 
-  and base_price > 500;
+SELECT DISTINCT 
+    product_name, 
+    product_code, 
+    promo_type, 
+    base_price 
+FROM 
+    revenue_ir_isu 
+WHERE 
+    promo_type = 'BOGOF' 
+    AND base_price > 500;
