07d00234ac76dead0e51fcc7002a43fe43ad0884
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:47:30 +0530

    Ad hoc 3.sql

--- Ad hoc code - Business Insights/Ad hoc 3.sql
+++ Ad hoc code - Business Insights/Ad hoc 3.sql
@@ -1,8 +1,8 @@
-select 
- campaign_name, 
- concat(round(sum(revenue_before_promo)/1000000,2), "M") as revenue_before_promo,
- concat(round(sum(revenue_after_promo)/1000000,2), "M") as revenue_after_promo
-from 
- revenue_ir_isu
-group by 
- campaign_name;
+SELECT 
+    campaign_name, 
+    CONCAT(ROUND(SUM(revenue_before_promo) / 1000000, 2), 'M') AS revenue_before_promo,
+    CONCAT(ROUND(SUM(revenue_after_promo) / 1000000, 2), 'M') AS revenue_after_promo
+FROM 
+    revenue_ir_isu
+GROUP BY 
+    campaign_name;
