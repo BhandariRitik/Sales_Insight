0293e4f17ca3b9bf53e1a303139ceca137b8e271
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:52:53 +0530

    Ad hoc 8.sql

--- Ad hoc code - Recommended Insights/Ad hoc 8.sql
+++ Ad hoc code - Recommended Insights/Ad hoc 8.sql
@@ -1,22 +1,16 @@
-/* product and category analysis */
 
-select 
- product_name, 
- category, 
- promo_type,
- round(
- (sum(IR)/sum(revenue_before_promo) * 100),2
- ) as revenue_incremental_percentage,
- round(
- (sum(isu)/sum(`quantity_sold(before_promo)`) *100),2
- ) as sold_qty_incremental_percentage
-
-from 
- revenue_ir_isu
-group by 
- product_name, 
- category, 
- promo_type
-order by 
- revenue_incremental_percentage desc,
- sold_qty_incremental_percentage desc
+SELECT 
+    product_name, 
+    category, 
+    promo_type,
+    ROUND((SUM(IR) / SUM(revenue_before_promo) * 100), 2) AS revenue_incremental_percentage,
+    ROUND((SUM(isu) / SUM(`quantity_sold(before_promo)`) * 100), 2) AS sold_qty_incremental_percentage
+FROM 
+    revenue_ir_isu
+GROUP BY 
+    product_name, 
+    category, 
+    promo_type
+ORDER BY 
+    revenue_incremental_percentage DESC,
+    sold_qty_incremental_percentage DESC;
