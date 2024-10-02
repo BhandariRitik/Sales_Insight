beedeed5fa03234ce9429edb8261dddf9a97c455
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:48:22 +0530

    Ad hoc 5.sql

--- Ad hoc code - Business Insights/Ad hoc 5.sql
+++ Ad hoc code - Business Insights/Ad hoc 5.sql
@@ -1,18 +1,18 @@
-with cte1 as (
- select 
-  product_name, 
-  category,
-  round((sum(IR)/sum(revenue_before_promo) * 100),2) as ir_pct
-from 
-  revenue_ir_isu
-group by 
-  Product_name, 
-  category
+WITH cte1 AS (
+    SELECT 
+        product_name, 
+        category,
+        ROUND((SUM(IR) / SUM(revenue_before_promo) * 100), 2) AS ir_pct
+    FROM 
+        revenue_ir_isu
+    GROUP BY 
+        product_name, 
+        category
 )
-select 
- * 
-from 
- cte1
-order by 
- ir_pct desc
-limit 5;
+SELECT 
+    * 
+FROM 
+    cte1
+ORDER BY 
+    ir_pct DESC
+LIMIT 5;
