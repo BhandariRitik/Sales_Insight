8a02d9adf080e7afa2b432950cb1be5a2268dd13
Ritik Bhandari <ritikbhandari004@gmail.com>
2024-10-02 18:49:39 +0530

    Ad hoc 3.sql

--- Ad hoc code - Recommended Insights/Ad hoc 3.sql
+++ Ad hoc code - Recommended Insights/Ad hoc 3.sql
@@ -1,12 +1,10 @@
-/* Promotion type analysis */
-
-select 
- Promo_type, 
- concat(round((sum(ir)/1000000),2), 'M') as incremental_revenue
-from 
- revenue_ir_isu
-group by 
- promo_type
-order by 
- sum(ir) desc
-limit 2;
+SELECT 
+    promo_type, 
+    CONCAT(ROUND((SUM(ir) / 1000000), 2), 'M') AS incremental_revenue 
+FROM 
+    revenue_ir_isu
+GROUP BY 
+    promo_type
+ORDER BY 
+    SUM(ir) DESC
+LIMIT 2;
